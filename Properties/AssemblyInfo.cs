44a81838af62dd45a8abfaf20910321154e7aa93
fulviofarina <fulviofarina@gmail.com>
2016-09-05 01:10:38 -0400

    fix 1

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rsx")]
+[assembly: AssemblyDescription("Resources Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Motion Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Fulvio Farina Arboccò, Copyright (c) 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74811b31-7277-4336-8fb0-73e2f2631795")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
---
+++ Tim.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Rsx
+{
+    public class Tim : Timer
+    {
+        public Tim(int seg, Action toDo)
+        {
+            this.Tick += new EventHandler(Tim_Tick);
+            this.Interval = seg * 1000;
+        }
+
+        private void Tim_Tick(object sender, EventArgs e)
+        {
+            if (afterTick == null) return;
+            afterTick.Invoke();
+            afterTick = null;
+            this.Dispose();
+        }
+
+        private Action afterTick;
+
+        public Action AfterTick
+        {
+            get { return afterTick; }
+            set { afterTick = value; }
+        }
+    }
+
+    /// <summary>
+    /// A Worker made for multiple methods
+    /// </summary>
+    ///
+}
