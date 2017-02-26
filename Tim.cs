3860c2899913a525e9ffffd41fdbcc74dd50975b
fulviofarina <fulviofarina@gmail.com>
2017-02-26 01:25:43 -0400

    v1.1

--- Tim.cs
+++ Tim.cs
@@ -3,33 +3,33 @@ using System.Windows.Forms;
 
 namespace Rsx
 {
-    public class Tim : Timer
+  public class Tim : Timer
+  {
+    public Tim(int seg, Action toDo)
     {
-        public Tim(int seg, Action toDo)
-        {
-            this.Tick += new EventHandler(Tim_Tick);
-            this.Interval = seg * 1000;
-        }
-
-        private void Tim_Tick(object sender, EventArgs e)
-        {
-            if (afterTick == null) return;
-            afterTick.Invoke();
-            afterTick = null;
-            this.Dispose();
-        }
-
-        private Action afterTick;
-
-        public Action AfterTick
-        {
-            get { return afterTick; }
-            set { afterTick = value; }
-        }
+      this.Tick += new EventHandler(Tim_Tick);
+      this.Interval = seg * 1000;
     }
 
-    /// <summary>
-    /// A Worker made for multiple methods
-    /// </summary>
-    ///
+    private void Tim_Tick(object sender, EventArgs e)
+    {
+      if (afterTick == null) return;
+      afterTick.Invoke();
+      afterTick = null;
+      this.Dispose();
+    }
+
+    private Action afterTick;
+
+    public Action AfterTick
+    {
+      get { return afterTick; }
+      set { afterTick = value; }
+    }
+  }
+
+  /// <summary>
+  /// A Worker made for multiple methods
+  /// </summary>
+  ///
 }
